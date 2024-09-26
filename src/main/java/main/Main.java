da739541a2c43c843f5694f488e46eefc05ade33
Kenneth <kennethchayes@gmail.com>
2024-09-26 17:47:18 -0400

    Testing

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -21,24 +21,3 @@ public class Main {
         myPet.drink();
     }
 }
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Wrath Fitness Pet!");
-
-        //Define root route for Spark 
-        get("/", (req, res) -> "Welcome to Wrath Fitness Pet");
-
-        // OAuth flow for Strava API use
-        System.out.println("Authorize the app by visiting : " + StravaAuth.getAuthUrl());
-        StravaAuth.startOAuthServer();
-
-        // Create a new pet
-        Pet myPet = new Pet("Fury");
-
-        // Interact with the pet
-        myPet.feed();
-        myPet.drink();
-    }
-}
