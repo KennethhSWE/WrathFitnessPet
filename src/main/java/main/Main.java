5227c047e00dea7b6d45e99fda8e9d5709bea9ca
Kenneth <kennethchayes@gmail.com>
2024-11-07 11:11:35 -0500

    front end redundacy

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         ipAddress("0.0.0.0");
 
-        staticFiles.location("/frontend");
+        staticFiles.location("/src/main/java/resources/frontend");
 
         mongoClient = MongoClients.create(System.getenv("MONGO_DB_URI"));
         MongoDatabase database = mongoClient.getDatabase("HeroAcademyGym");
