cc734ed07a203c9f448d8139d7e06ea92615882a
Kenneth <kennethchayes@gmail.com>
2024-11-07 10:04:41 -0500

    added the ability for render to use static files

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         ipAddress("0.0.0.0");
 
-        //staticFiles.externalLocation("frontend");
+        staticFiles.externalLocation("frontend");
 
         mongoClient = MongoClients.create(System.getenv("MONGO_DB_URI"));
         MongoDatabase database = mongoClient.getDatabase("HeroAcademyGym");
